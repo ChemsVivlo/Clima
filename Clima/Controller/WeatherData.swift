27e3d5aa1dabe89aed8853b98aea826f6c6c7472
Jose Maria Vivanco <jm.vivlo@gmail.com>
2024-01-18 12:03:31 -0600

    Develop app to retrieve the weather conditions depending on user's location or typed city.

---
+++ Clima/Controller/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Jose Vivanco on 17/01/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Controller/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Jose Vivanco on 18/01/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
